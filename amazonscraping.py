159ea019cada24b23b6aa5afbd6776c3b4314b08
am1th-hegde <amithhegde5@gmail.com>
2024-01-25 05:16:37 +0530

    updated with direct search

--- amazonscraping.py
+++ amazonscraping.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[14]:
-
-
-# pip install selenium
-
-
-# In[15]:
-
-
-# pip install webdriver-manager
-
-
-# In[3]:
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -23,8 +5,6 @@ from selenium.webdriver.chrome.service import Service
 import time
 import random
 
-# Replace 'your_website_url' with the actual URL of the website you want to scrape
-website_url = 'https://www.amazon.in/Huion-Inspiroy-Graphics-Battery-Free-Sensitivity/dp/B07R7L3HY7/ref=pd_ci_mcx_mh_mcx_views_0?pd_rd_w=eKxJN&content-id=amzn1.sym.cd312cd6-6969-4220-8ac7-6dc7c0447352%3Aamzn1.symc.ca948091-a64d-450e-86d7-c161ca33337b&pf_rd_p=cd312cd6-6969-4220-8ac7-6dc7c0447352&pf_rd_r=A4SGA5JPZ8NZ5R2KRM0M&pd_rd_wg=NjqWX&pd_rd_r=bdbbf170-6850-4f8b-b1d0-f96ec61be264&pd_rd_i=B07R7L3HY7'
 
 # Set up the Selenium WebDriver (make sure to download the appropriate driver for your browser)
 # For Chrome, download chromedriver: https://sites.google.com/chromium.org/driver/
@@ -34,8 +14,27 @@ service = Service(executable_path='./chromedriver.exe')
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox()
 
+driver.get("https://www.amazon.in/")
+
+# Find the search input field and enter your search query
+search_box = driver.find_element(By.CSS_SELECTOR,'#twotabsearchtextbox')
+search_query = input("Enter product name: ")  # Replace with the product you want to search
+search_box.send_keys(search_query)
+search_box.send_keys(Keys.RETURN)
+
+# Wait for the search results to load
+time.sleep(2)  # You might need to adjust the wait time based on your internet speed
+
+# Find the first search result and get its URL
+try:
+    first_result = driver.find_element(By.CSS_SELECTOR,'.widgetId\=search-results_3 > span:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(2) > a:nth-child(1)')
+    product_url = first_result.get_attribute("href")
+except Exception as e:
+    print("Error:", e)
+
+
 # Open the website
-driver.get(website_url)
+driver.get(product_url)
 delay_between_requests = 3
 #random.uniform(4, 6)
 time.sleep(delay_between_requests)
@@ -51,10 +50,7 @@ numbers_text = number_element.text
 driver.quit()
 
 # Print the scraped numbers
-print(f"Scraped Numbers: {numbers_text}")
-
-
-# In[ ]:
+print(f"Price: ₹ {numbers_text}")
 
 
 
