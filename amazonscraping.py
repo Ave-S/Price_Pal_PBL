654e4a5392b7a0b9e6ff2caf4c1bad09e8b13362
am1th-hegde <amithhegde5@gmail.com>
2024-01-27 16:20:20 +0530

    updated with keys

--- amazonscraping.py
+++ amazonscraping.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 # from webdriver.manager.chrome import ChromeDriverManager
 import time
 import random
